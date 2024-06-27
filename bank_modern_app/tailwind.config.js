01992e8ad6b3c51d4a6e4b2e416e1341892d1cb2
Hasibullah Wahabzadah <hasibullahwahabzadah@gmail.com>
2024-06-27 11:11:19 +0430

    frist-step

--- bank_modern_app/tailwind.config.js
+++ bank_modern_app/tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports = {
+  content: ["./index.html", "./scr/**/*.{js, jsx}"],
+mode: "jit",
+theme: {
+  extend: {
+    colors: {
+      primary: "#00040f",
+      secondary: " #00f6ff",
+      dimWhite:"rgba(255,255,255,0.7)",
+      dimBlue: "rgba(9,151,124,0.1)",
 
+    },
+    fontFamily:{
+      poppins:["poppins", "sans_serif"],
+    },
+  },
+  screens: {
+    xs: "480px",
+    ss: "620px",
+    sm: "768px",
+    md: "1060px",
+    lg: "1200px",
+    xl: "1700px"
+  },
+},
+plugins: [],
+}
