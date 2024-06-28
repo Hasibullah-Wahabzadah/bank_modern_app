57b2b3e0c1629948e55126aad8dae0ec08cc004a
Hasibullah Wahabzadah <hasibullahwahabzadah@gmail.com>
2024-06-28 11:16:00 +0430

    components || style.js

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-const App = () => {
-  return (
-    <div>App</div>
+const App = () => 
+  (
+    <div className="bg-primary w-full overflow-hidden">
+      <div>
+        <h1></h1>
+      </div>
+    </div>
 
   ) 
-}
  
 export default App
---
+++ bank_modern_app/src/components/business.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function business() {
+  return (
+    <div>business</div>
+  )
+}
+
+export default business
